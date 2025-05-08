78672c2747868685b8c57d304ddba6ad03e5d1b4
hayrapetyansami <samihayrapetyan@gmail.com>
2025-05-08 22:25:45 +0400

    Created React with VITE and added TailwindCSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center text-center bg-gray-500 dark:bg-[#112d49]">
+      <header className="bg-gray-600 dark:bg-[#0a1a2d] text-white w-full mb-4">
+        <h1 className="text-4xl my-8">Book Library App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App
