7ed55e3bc0c53603bfe948d17ff1a54b86f3a419
hayrapetyansami <samihayrapetyan@gmail.com>
2025-05-10 22:40:32 +0400

    added json file with books and create random book function

--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { v4 as uuidv4 } from 'uuid';
 
 import { addBook } from "../../redux/books/actionCreators";
+import data from "../../data/books.json";
 
 const Form = () => {
   const [title, setTitle] = useState("");
@@ -17,7 +18,15 @@ const Form = () => {
       setTitle("");
       setAuthor("");
     }
-  }
+  };
+
+  const handleAddRandonBook = () => {
+    const rndIndex = Math.floor(Math.random() * data.length);
+    const rndBook = data[rndIndex];
+    dispatch(addBook({ ...rndBook, id: uuidv4() }));
+  };
+
+
   return (
     <div className="p-4 m-4 bg-[#f2f2f2] rounded-lg shadow-lg">
       <h2 className="mb-4 text-2xl text-center">Add a New Book</h2>
@@ -36,9 +45,27 @@ const Form = () => {
           className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-lg"
           placeholder="Book Author"
         />
-        <button  className="block w-full bg-[#007bff] text-white py-2 px-4 rounded-lg cursor-pointer hover:bg-[#0056b3] transition-colors duration-300">
-          Click Me
-        </button>
+        <div className="flex gap-4 max-lg:flex-wrap">
+          <button
+            type="submit"
+            className="max-md:basis-full max-lg:basis-1/3 flex-1 bg-[#007bff] text-white text-sm py-2 px-4 rounded-lg cursor-pointer hover:bg-[#0056b3] transition-colors duration-300"
+          >
+            Add Book
+          </button>
+          <button
+            onClick={handleAddRandonBook}
+            type="button"
+            className="max-md:basis-full max-lg:basis-1/3 flex-1 bg-[#007bff] text-white text-sm py-2 px-4 rounded-lg cursor-pointer hover:bg-[#0056b3] transition-colors duration-300"
+          >
+            Random
+          </button>
+          <button
+            type="button"
+            className="max-md:basis-full max-lg:basis-1/3 flex-1 bg-[#007bff] text-white text-sm py-2 px-4 rounded-lg cursor-pointer hover:bg-[#0056b3] transition-colors duration-300"
+          >
+            Add Book
+          </button>
+        </div>
       </form>
     </div>
   );
--- frontend/src/components/list/List.jsx
+++ frontend/src/components/list/List.jsx
@@ -6,9 +6,9 @@ const List = () => {
   const books = useSelector(state => state.books);
 
   return (
-    <div className="p-4 m-4 bg-[#f2f2f2] rounded-lg shadow-lg">
+    <div className="p-4 m-4 bg-[#f2f2f2] rounded-lg shadow-lg min-h-[145px]">
       <h2 className="text-center">Book List</h2>
-      <ul className="p-0 m-4">
+      <ul className="p-0 m-4 max-h-[520px] overflow-y-auto">
         {
           !books.length ?
             <p className="text-center">No books available</p>
