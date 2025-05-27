dec3b8f483e28857725ea42667074a74e71d0de9
hayrapetyansami <samihayrapetyan@gmail.com>
2025-05-27 21:40:11 +0400

    create sliceError and added it to store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 // import booksReducer from "./books/reducer";
 import filterReducer from "./slices/filterSlice";
 import booksReducer from "./slices/booksSlice";
+import errorReducer from "./slices/errorSlice";
 
 const store = configureStore({
   reducer: {
     // books: booksReducer,
     books: booksReducer,
-    filter: filterReducer
+    filter: filterReducer,
+    error: errorReducer
   }
 });
 
